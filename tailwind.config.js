6504f6ea1b1e939b79e7101c5c866d3fc121bdbc
lonelybuddy <henryzheng92@gmail.com>
2022-03-04 16:20:33 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    fontSize: {
+      xs: ['.6875rem', { letterSpacing: '.216rem', lineHeight: '.9375rem' }],
+      sm: ['.8125rem', { letterSpacing: '.255rem', lineHeight: '1.11rem' }],
+      md: ['1.5rem', { letterSpacing: '.01625rem', lineHeight: '2.048rem' }],
+      lg: ['1.75rem', { letterSpacing: '.01875rem', lineHeight: '2.39rem' }],
+    },
+    extend: {
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
+      },
+      colors: {
+        'ligh-Cyan': 'hsl(193, 38%, 86%)',
+        'Neon Green': 'hsl(150, 100%, 66%)',
+        'Grayish Blue': 'hsl(217, 19%, 38%)',
+        'Dark Grayish Blue': 'hsl(217, 19%, 24%)',
+        'Dark Blue': 'hsl(218, 23%, 16%)',
+      },
+      boxShadow: {
+        glowing: '0px 0px 40px #53FFAA',
+      },
+    },
+  },
+  plugins: [],
+};
